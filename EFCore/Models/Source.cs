7809a0afcf4e56c8df2e8ab260182db9c30659b1
Courtney Cloudman <1638412+chirpycora@users.noreply.github.com>
2024-07-13 21:52:27 -0500

    Update schema of Source

--- EFCore/Models/Source.cs
+++ EFCore/Models/Source.cs
@@ -5,14 +5,14 @@ namespace CC.BabyNameDb.EFCore.Models
 		public int Id { get; set; }
 
 		/// <summary>
+		/// The key used to identify the data source, used for processing
+		/// </summary>
+		public string Key { get; set; } = string.Empty;
+
+		/// <summary>
 		/// Whether the data source is being actively synced
 		/// </summary>
 		public bool Active { get; set; }
-
-		/// <summary>
-		/// The copyright that applies to the data source
-		/// </summary>
-		public string Copyright { get; set; } = string.Empty;
 
 		/// <summary>
 		/// A description of the data source
@@ -31,6 +31,21 @@ namespace CC.BabyNameDb.EFCore.Models
 		/// <summary>
 		/// The date of the last update, as specified by the data provider
 		public DateTime? LastUpdatedUtc { get; set; }
+
+		/// <summary>
+		/// The copyright that applies to the data source
+		/// </summary>
+		public string LicenseName { get; set; } = string.Empty;
+
+		/// <summary>
+		/// The URL for the license that applies to the data source
+		/// </summary>
+		public string LicenseUrl { get; set; } = string.Empty;
+
+		/// <summary>
+		/// The name of the organization that provides the data
+		/// </summary>
+		public string OrganizationName { get; set; } = string.Empty;
 
 		/// <summary>
 		/// The public URL of the data source
